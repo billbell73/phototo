fcc7d048fa4b1ab2a274d66ee5347659fb5e1c07
Will Bell <willbell@gmail.com>
2014-02-19 18:40:29 +0000

    Production env changes for sending email via mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Phototo::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -86,5 +86,16 @@ Phototo::Application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  config.action_mailer.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'yourapp.heroku.com',
+  :authentication => :plain,
+  }
+  
+  config.action_mailer.delivery_method = :smtp
   
 end
